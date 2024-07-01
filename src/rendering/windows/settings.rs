f587e63b7a8ce52612af460fef1a314db39bd9c5
Pandicon <70060103+Pandicon@users.noreply.github.com>
2024-07-01 12:02:55 +0200

    Add planet rotation and make Marco sleep at night

--- src/rendering/windows/settings.rs
+++ src/rendering/windows/settings.rs
@@ -13,17 +13,17 @@ impl application::Application {
 			ui.separator();
 			ui.heading("Marco parameters");
 			ui.horizontal(|ui| {
-				anything_changed |= ui.add(egui::DragValue::new(&mut self.settings.marco_velocity)).changed();
+				anything_changed |= ui.add(egui::DragValue::new(&mut self.settings.marco_velocity).speed(0.01)).changed();
 				ui.label("Marco's velocity (m/s)");
 			});
 			ui.horizontal(|ui| {
 				anything_changed |= ui.drag_angle(&mut self.settings.start_lat).changed();
-				self.settings.start_lat = self.settings.start_lat.clamp(-PI/2.0, PI / 2.0);
+				self.settings.start_lat = self.settings.start_lat.clamp(-PI / 2.0, PI / 2.0);
 				ui.label("Starting latitude (deg)");
 			});
 			ui.horizontal(|ui| {
 				anything_changed |= ui.drag_angle(&mut self.settings.start_lon).changed();
-				self.settings.start_lon = self.settings.start_lon.clamp(-PI/2.0, PI / 2.0);
+				self.settings.start_lon = self.settings.start_lon.clamp(-PI / 2.0, PI / 2.0);
 				ui.label("Starting longitude (deg)");
 			});
 			ui.separator();
@@ -32,6 +32,16 @@ impl application::Application {
 				anything_changed |= ui.add(egui::DragValue::new(&mut self.settings.planet_radius)).changed();
 				self.settings.planet_radius = self.settings.planet_radius.max(10.0_f64.powi(-6));
 				ui.label("Planet radius (km)");
+			});
+			ui.horizontal(|ui| {
+				anything_changed |= ui.drag_angle(&mut self.settings.rotational_axis_tilt).changed();
+				self.settings.rotational_axis_tilt = self.settings.rotational_axis_tilt.clamp(-PI, PI);
+				ui.label("Rotational axis tilt (deg)").on_hover_text("The tilt of the rotational axis of the planet, measured from the normal to the ecliptic. Positive values make it point towards the star at the start, negative away.");
+			});
+			ui.horizontal(|ui| {
+				anything_changed |= ui.add(egui::DragValue::new(&mut self.settings.rotational_period)).changed();
+				self.settings.rotational_period = self.settings.rotational_period.max(10.0_f64.powi(-6));
+				ui.label("Sideric rotation period (h)");
 			});
 			ui.separator();
 			ui.heading("Planet orbit parameters");
@@ -43,16 +53,16 @@ impl application::Application {
 			ui.separator();
 			ui.heading("Simulation parameters");
 			ui.horizontal(|ui| {
-				anything_changed |= ui.add(egui::DragValue::new(&mut self.settings.timestep)).changed();
+				anything_changed |= ui.add(egui::DragValue::new(&mut self.settings.timestep).speed(0.01)).changed();
 				ui.label("Timestep (s)");
 			});
 			ui.horizontal(|ui| {
-				anything_changed |= ui.add(egui::DragValue::new(&mut self.settings.simulation_time)).changed();
+				anything_changed |= ui.add(egui::DragValue::new(&mut self.settings.simulation_time).speed(100.0)).changed();
 				ui.label("Time to simulate (s)");
 			});
 			ui.horizontal(|ui| {
 				anything_changed |= ui.add(egui::DragValue::new(&mut self.settings.points_to_show)).changed();
-				ui.label("Number of points to show in the graph (approximate value, usually ± 1");
+				ui.label("Number of points to show in the graph (approximate value, usually ± 1)");
 			});
 			if self.settings.recalculate_on_change && anything_changed {
 				self.recalculate();
--- src/settings.rs
+++ src/settings.rs
@@ -2,10 +2,14 @@ pub struct Settings {
     pub recalculate_on_change: bool,
 
     pub marco_velocity: f64,
-    pub planet_radius: f64,
-    pub sun_distance: f64,
     pub start_lat: f32,
     pub start_lon: f32,
+
+    pub planet_radius: f64,
+    pub rotational_axis_tilt: f32,
+    pub rotational_period: f64,
+
+    pub sun_distance: f64,
 
     pub timestep: f64,
     pub simulation_time: f64,
@@ -16,11 +20,17 @@ impl Default for Settings {
     fn default() -> Self {
         Self {
             recalculate_on_change: false,
+
             marco_velocity: 5.0 / 3.6,
-            planet_radius: 6000.0,
-            sun_distance: 150.0 * 10.0_f64.powi(6),
             start_lat: 89.7_f32.to_radians(),
             start_lon: -50.0_f32.to_radians(),
+
+            planet_radius: 6000.0,
+            rotational_axis_tilt: 0.0_f32.to_radians(),
+            rotational_period: 24.0,
+
+            sun_distance: 150.0 * 10.0_f64.powi(6),
+
             timestep: 1.0,
             simulation_time: 86400.0,
             points_to_show: 1000,
